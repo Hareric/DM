c252b27d20a760e8c0b4178405d4d0bd3545c77f
Eric Chan <zero@mailir.com>
2015-05-17 17:26:56 +0800

    Computer Robot
    先收集新闻网址，在对每一条新闻新建一个txt，每个txt存入标题，正文，时间，关键字

--- 新闻爬取.py
+++ 新闻爬取.py
@@ -30,7 +30,7 @@ print len(a)
 
 file1 = open(r'News_url.txt')
 url_line = 1
-while url_line:
+for i in range(500):
      url_line = file1.readline()
      text = urllib.urlopen(url_line).read() #读取网址的源代码
      charset = chardet.detect(text) #检测文件编码
@@ -50,14 +50,16 @@ while url_line:
      pattern3 = re.compile('<p>(.*)</p>')
      description = re.findall(pattern3,text)
 
+
      #新建txt 存入标题 时间 关键字 正文
      file2 = open('NEWS/%s.txt'%title[0],'w') #新建文件名为标题的txt
      file2.write('标题:' + title[0] + '\n') #输入标题 换行
      file2.write('时间:' + time[0] + '\n') #输入时间 换行
      file2.write('关键字:' + keyword[0] + '\n')#输入关键字换行
      file2.write('正文:'+'\n')
-     for c in range(len(description)):
-         file2.write(description[c]+'\n')#输入正文
+     for c in range(len(description) - 7): #忽略最后产生的7行乱码
+         temp = re.sub('<strong>|</strong>','',description[c]) #将出现在正文的<strong>和</strong>删除掉
+         file2.write(temp+'\n')#输入正文
 
      file2.close()
 
